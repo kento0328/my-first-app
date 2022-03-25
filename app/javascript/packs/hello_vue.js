83c8ff514c2b359abb46ad819c5e76796321573a
kento0328 <usa.kento328@gmail.com>
2022-03-25 12:06:04 +0900

    ready to go

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log('this is hello_vue.js')
   });
+
+   import { createApp } from "vue";
+ import App from "../app.vue";
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const app = createApp(App);
+    app.mount("#vue-app");
+  });
