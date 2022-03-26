e3be80ea9debcb63a92e18be0ec4cfd60d447a6e
kento0328 <usa.kento328@gmail.com>
2022-03-26 21:51:31 +0900

    first API

--- app/javascript/components/home/Index.vue
+++ app/javascript/components/home/Index.vue
@@ -1,24 +1,19 @@
 <template>
   <div>
-    <h1>{{ initialData.title }}</h1>
-    <p>{{ initialData.description }}</p>
-    <table class="contents-table">
-      <tr>
-        <th>名前</th>
-        <th>リンク</th>
-      </tr>
-      <tr v-for="outer_link in initialData.contents.outer_links" v-bind:key="outer_link.name">
-        <td>{{ outer_link.name }}</td>
-        <td>
-          <a v-bind:href="outer_link.url">{{ outer_link.text }}</a>
-        </td>
-      </tr>
-    </table>
+     <HeaderView v-bind:initialData.title="title" v-bind:description="initialData.description" />
+     <Contents v-bind:contents="initialData.contents" />
   </div>
 </template>
 
 <script>
+ import HeaderView from "../HeaderView.vue";
+ import Contents from "./Contents.vue";
+
 export default {
+    components: {
+     HeaderView,
+     Contents
+   },
   props: {
     initialData: {
       type: Object,
@@ -28,13 +23,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.contents-table {
-  border: 1px solid gray;
-  margin: 10px;
-}
-.contents-table th,
-.contents-table td {
-  border: 1px solid gray;
-}
-</style>
+<style scoped></style>
